8e332e884b0c5251182ed22ad3a486e34fbdd250
jan <janvanbergen16@gmail.com>
2019-10-14 16:25:27 +0200

    Added comments

--- SudokuEstimator++/SudokuEstimator.h
+++ SudokuEstimator++/SudokuEstimator.h
@@ -27,14 +27,18 @@ private:
 	static constexpr int coordinate_count = Sudoku<N>::size * (Sudoku<N>::size - N);
 	int coordinates[coordinate_count];
 
+	// Variables used for uniform random number generation
 	std::random_device random_device;
 	std::mt19937       rng;
 
+	// Uses backtracking to count all possible valid Sudoku solutions, given the current configuration of the grid
 	void backtrack_with_forward_check(Big_Integer& total);
 
 	// Takes a random walk of length s through the tree of all possible Sudokus
 	void knuth(Big_Integer& result);
 
+	// Gives and estimate of the amount of valid Sudoku grids,
+	// using a combination of Knuth's algorithm and backtracking
 	void estimate_solution_count(Big_Integer& estimate, Big_Integer& backtrack);
 
 public:
