b5a1a58a6dd6b829c1e55a6a93a294abb9c64b3b
jan <janvanbergen16@gmail.com>
2019-10-19 17:11:53 +0200

    Cleanup

--- SudokuEstimator++/Python Scripts/process_results.py
+++ SudokuEstimator++/Python Scripts/process_results.py
@@ -7,6 +7,8 @@ file_path = '../Results/results_3x3_s=8.txt'
 latin_rectangle_count = 2102110586634240
 true_sudoku_count     = 6670903752021072936960
 
+print('Reading file...')
+
 estimates = []
 
 # Load a results file line by line and store it in the array 'estimates'
@@ -17,24 +19,27 @@ with open(file_path) as file:
        estimates.append(int(line))
        line = file.readline()
 
+print('Transforming estimates...')
+
 cumulative_sum = 0
 n = 0
 
-cumulative_average = []
+running_average = []
 
 # Multiply each estimate with the number of M x N*M Latin Rectangles
+# Calculate the running average of these estimates
 for estimate in estimates:
     total_estimate = estimate * latin_rectangle_count
 
     cumulative_sum += total_estimate
     n += 1
 
-    cumulative_average.append(cumulative_sum / n)
+    running_average.append(cumulative_sum / n)
 
 # Plot the data
-plt.plot(cumulative_average)
+plt.plot(running_average)
 plt.hlines(true_sudoku_count, xmin=0, xmax=n, linestyles='dashed')
 plt.xlabel('Iteration Number')
 plt.ylabel('Estimate')
-plt.title('Sudoku Solution Estimate - Cumulative Average')
+plt.title('Sudoku Solution Estimate - Running Average')
 plt.show()
